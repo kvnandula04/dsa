2a202b375aa1f5eebc6ff50a058b17357bf0a009
Karthik Nandula <kvnandula04@gmail.com>
2025-06-20 23:28:40 +0100

    Solve valid parentheses

---
+++ Grind75/valid_parentheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        brackets = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for c in s:
+            if c in brackets.values(): # open bracket
+                stack.append(c)
+            
+            elif c in brackets.keys(): # closed bracket
+                if not stack or stack[-1] != brackets[c]:
+                    return False
+                stack.pop()
+
+        return len(stack) == 0
+
+
+# Test
+sol = Solution()
+
+solution = sol.isValid("()")
+print(solution)  # Expected Output: True
+solution = sol.isValid("()[]{}")
+print(solution)  # Expected Output: True
+solution = sol.isValid("(]")
+print(solution)  # Expected Output: False
+solution = sol.isValid("([])")
+print(solution)  # Expected Output: True
